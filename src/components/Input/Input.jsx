c7195dd90b565091d1ee78d29a1e8845516b1234
Dmytro Yevich <dmytro.yevich@cengage.com>
2026-05-11 12:45:14 +0300

    Add New Project Menu and Input components; style it with Tailwind

---
+++ src/components/Input/Input.jsx
@@ -0,0 +1,8 @@
+export default function Input({ isTextarea, label, ...props }) {
+    return (
+        <p className="flex flex-col my-4 gap-1">
+            <label className="text-sm text-stone-500 font-bold uppercase">{label}</label>
+            {isTextarea ? <textarea className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" {...props} /> : <input {...props} />}
+        </p>
+    );
+}
---
+++ src/components/NewProjectMenu/NewProjectMenu.jsx
@@ -0,0 +1,22 @@
+import Input from '../Input/Input';
+
+export default function NewProjectMenu() {
+    return (
+        <div className="w-[35rem] mt-16">
+            <menu className="flex items-center justify-end gap-4 my-4">
+                <li>
+                    <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+                </li>
+                <li>
+                    <button className="px-6 py-2 rounded-md bg-stone-800 hover:bg-stone-950 text-stone-50 hover:text-stone-200">Save</button>
+                </li>
+            </menu>
+
+            <div>
+                <Input label="Title" />
+                <Input isTextarea label="Description" />
+                <Input label="Due Date" />
+            </div>
+        </div>
+    );
+}
