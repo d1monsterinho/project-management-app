aa5d2e3050049c95407f9bc5de8e78a3833df53e
Dmytro Yevich <dmytro.yevich@cengage.com>
2026-05-11 13:30:56 +0300

    Implement placeholder for no project selected; extract button in a separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import ProjectsSidebar from "./components/ProjectsSidebar/ProjectsSidebar";
 import NewProjectMenu from "./components/NewProjectMenu/NewProjectMenu";
+import NoProjectSelectedPlaceholder from "./components/NoProjectSelectedPlaceholder/NoProjectSelectedPlaceholder";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar />
+      <NoProjectSelectedPlaceholder />
     </main>
   );
 }
