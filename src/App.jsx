e2f9ebd6c6132a3e6c236005750318ddaa5e8201
Dmytro Yevich <dmytro.yevich@cengage.com>
2026-05-11 13:50:16 +0300

    Implement conditional rendering when add project button was pressed and wasn't

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
+import { useState } from "react";
+
 import ProjectsSidebar from "./components/ProjectsSidebar/ProjectsSidebar";
 import NewProjectMenu from "./components/NewProjectMenu/NewProjectMenu";
 import NoProjectSelectedPlaceholder from "./components/NoProjectSelectedPlaceholder/NoProjectSelectedPlaceholder";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function onAddProjectClick() {
+    setProjectsState(prev => {
+      return ({
+        ...prev,
+        selectedProjectId: null,
+      });
+    });
+  }
+
+  function onNewProjectMenuCancelClick() {
+    setProjectsState(prev => {
+      return ({
+        ...prev,
+        selectedProjectId: undefined,
+      });
+    });
+  }
+
+  const content = projectsState.selectedProjectId === null ?
+    <NewProjectMenu onCancelClick={onNewProjectMenuCancelClick}/>
+    :
+    <NoProjectSelectedPlaceholder onCreateProjectClick={onAddProjectClick}/>;
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelectedPlaceholder />
+      <ProjectsSidebar onAddProjectClick={onAddProjectClick}/>
+      {content}
     </main>
   );
 }
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,8 +1,10 @@
 export default function Input({ isTextarea, label, ...props }) {
+    const inputClasses = 'w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600';
+    
     return (
         <p className="flex flex-col my-4 gap-1">
             <label className="text-sm text-stone-500 font-bold uppercase">{label}</label>
-            {isTextarea ? <textarea className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" {...props} /> : <input {...props} />}
+            {isTextarea ? <textarea className={inputClasses} {...props} /> : <input className={inputClasses} {...props} />}
         </p>
     );
 }
