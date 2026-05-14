18bd04e648ae38fb2233b9226c880cfa30c69ebc
Dmytro Yevich <dmytro.yevich@cengage.com>
2026-05-14 15:20:54 +0300

    Implement project creation/deletion

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import ProjectsSidebar from "./components/ProjectsSidebar/ProjectsSidebar";
 import NewProjectMenu from "./components/NewProjectMenu/NewProjectMenu";
 import NoProjectSelectedPlaceholder from "./components/NoProjectSelectedPlaceholder/NoProjectSelectedPlaceholder";
+import SelectedProject from "./components/SelectedProject/SelectedProject";
 
 function App() {
   const [projectsState, setProjectsState] = useState({
@@ -28,7 +29,7 @@ function App() {
         dueDate: dueDate,
       };
 
-      return({
+      return ({
         selectedProjectId: undefined,
         projects: [...prev.projects, newProject],
       });
@@ -44,14 +45,38 @@ function App() {
     });
   }
 
-  const content = projectsState.selectedProjectId === null ?
-    <NewProjectMenu onSaveClick={onNewProjectMenuSaveClick} onCancelClick={onNewProjectMenuCancelClick}/>
-    :
-    <NoProjectSelectedPlaceholder onCreateProjectClick={onAddProjectClick}/>;
+  function onSelectProjectClick(id) {
+    setProjectsState(prev => ({
+      ...prev,
+      selectedProjectId: id,
+    }));
+  }
+
+  function onDeleteProjectButtonClick(id) {
+    setProjectsState(prev => ({
+      selectedProjectId: undefined,
+      projects: prev.projects.filter(project => project.id !== id),
+    }))
+  }
+
+  let content = <NoProjectSelectedPlaceholder onCreateProjectClick={onAddProjectClick} />;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProjectMenu onSaveClick={onNewProjectMenuSaveClick} onCancelClick={onNewProjectMenuCancelClick} />;
+  } else if (projectsState.selectedProjectId) {
+    content = <SelectedProject
+     project={projectsState.projects.find(project => project.id === projectsState.selectedProjectId)}
+     onDeleteClick={onDeleteProjectButtonClick}
+     />
+  }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onAddProjectClick={onAddProjectClick} projectsState={projectsState} />
+      <ProjectsSidebar
+        onAddProjectClick={onAddProjectClick}
+        projectsState={projectsState}
+        handleSelectProject={onSelectProjectClick}
+      />
       {content}
     </main>
   );
